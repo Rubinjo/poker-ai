59a9cbc7a3e287ca6ec2df94c444c538c965db41
Ruben Lucas <ruben.lucas1997@gmail.com>
2022-05-07 01:13:05 +0200

    ✨ Pytorch DQN setup

---
+++ ai/dqn.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+class DQN(nn.Module):
+    def __init__(self, input_dim: int, output_dim: int):
+        super().__init()
+        self.fc1 == nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        return self.fc1(x)
